854c115fab671a9355487a6391d1188c41dc2e2a
James Canning <james@brudil.com>
2019-10-24 18:38:12 +0100

    Decent nav in ProjectSidebar Closes #21

--- packages/client/src/containers/ApplicationContainer.tsx
+++ packages/client/src/containers/ApplicationContainer.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
 import {StatusBar} from "../components/StatusBar";
 import {ProjectSidebar} from "../components/ProjectSidebar";
 import {Global} from "@emotion/core";
@@ -6,23 +9,25 @@ import {Editor} from "../components/Editor";
 
 export const ApplicationContainerComponent: React.FC = () => {
   return (
-    <div>
-      <Global styles={{
-        body: {
-        margin: 0,
-        padding: 0,
-        fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-      }
-      }} />
-      <div css={{
-        display: 'flex',
-      }}>
-        <ProjectSidebar />
-        <div css={{ flex: 'auto', marginLeft: '1rem' }}>
-          <StatusBar />
-          <Editor />
+    <Router>
+      <div>
+        <Global styles={{
+          body: {
+          margin: 0,
+          padding: 0,
+          fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+        }
+        }} />
+        <div css={{
+          display: 'flex',
+        }}>
+          <ProjectSidebar />
+          <div css={{ flex: 'auto', marginLeft: '1rem' }}>
+            <StatusBar />
+            <Editor />
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }

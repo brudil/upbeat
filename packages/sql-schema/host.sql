2ffb63e4eb6a1688f8c483fc62a2753352ff8bf2
James Canning <james@brudil.com>
2019-10-29 22:57:58 +0000

    Add defaults to schema

--- packages/sql-schema/host.sql
+++ packages/sql-schema/host.sql
@@ -11,59 +11,51 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
+
+
 SET default_with_oids = false;
+
+
+CREATE TABLE public.episodes (
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    name text NOT NULL,
+    show_id uuid NOT NULL
+);
+
 
 
 CREATE TABLE public.organisations (
     name text NOT NULL,
     created_at timestamp without time zone NOT NULL,
-    id uuid NOT NULL
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL
 );
 
 
 
 CREATE TABLE public.shows (
-    name text NOT NULL,
-    id uuid NOT NULL
+    name text DEFAULT public.gen_random_uuid() NOT NULL,
+    id uuid NOT NULL,
+    organisation_id uuid NOT NULL
 );
-
-
-
-CREATE TABLE public.shows_to_organisations (
-    id integer NOT NULL,
-    show uuid NOT NULL,
-    organisation uuid NOT NULL
-);
-
-
-
-CREATE SEQUENCE public.shows_to_organisations_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-
-ALTER SEQUENCE public.shows_to_organisations_id_seq OWNED BY public.shows_to_organisations.id;
 
 
 
 CREATE TABLE public.users (
-    id uuid NOT NULL,
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
     first_name text NOT NULL,
     last_name text NOT NULL,
     password_hash text NOT NULL,
     email_address text NOT NULL,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 
 
-ALTER TABLE ONLY public.shows_to_organisations ALTER COLUMN id SET DEFAULT nextval('public.shows_to_organisations_id_seq'::regclass);
+ALTER TABLE ONLY public.episodes
+    ADD CONSTRAINT episodes_pkey PRIMARY KEY (id);
 
 
 
@@ -77,23 +69,18 @@ ALTER TABLE ONLY public.shows
 
 
 
-ALTER TABLE ONLY public.shows_to_organisations
-    ADD CONSTRAINT shows_to_organisations_pkey PRIMARY KEY (id);
-
-
-
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 
 
 
-ALTER TABLE ONLY public.shows_to_organisations
-    ADD CONSTRAINT shows_to_organisations_organisation_fkey FOREIGN KEY (organisation) REFERENCES public.organisations(id);
+ALTER TABLE ONLY public.episodes
+    ADD CONSTRAINT episodes_show_id_fkey FOREIGN KEY (show_id) REFERENCES public.shows(id);
 
 
 
-ALTER TABLE ONLY public.shows_to_organisations
-    ADD CONSTRAINT shows_to_organisations_show_fkey FOREIGN KEY (show) REFERENCES public.shows(id);
+ALTER TABLE ONLY public.shows
+    ADD CONSTRAINT shows_organisation_id_fkey FOREIGN KEY (organisation_id) REFERENCES public.organisations(id);
 
 
 

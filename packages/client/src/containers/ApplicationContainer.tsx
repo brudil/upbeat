c791fb176e7420cc6c7d3731f27b4ec04e635d6c
James Canning <james@brudil.com>
2019-10-29 19:09:57 +0000

    Add Rush to client app

--- packages/client/src/containers/ApplicationContainer.tsx
+++ packages/client/src/containers/ApplicationContainer.tsx
@@ -1,33 +1,42 @@
 import React from 'react';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
-import {StatusBar} from "../components/StatusBar";
-import {ProjectSidebar} from "../components/ProjectSidebar";
-import {Global} from "@emotion/core";
-import {Editor} from "../components/Editor";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { StatusBar } from '../components/StatusBar';
+import { ProjectSidebar } from '../components/ProjectSidebar';
+import { Global } from '@emotion/core';
+import { Editor } from '../components/Editor';
+import { RushProvider } from '../../../rush/src/context';
+import { createRush } from '../../../rush/src/rush';
+
+const rush = createRush();
 
 export const ApplicationContainerComponent: React.FC = () => {
   return (
-    <Router>
-      <div>
-        <Global styles={{
-          body: {
-          margin: 0,
-          padding: 0,
-          fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-        }
-        }} />
-        <div css={{
-          display: 'flex',
-        }}>
-          <ProjectSidebar />
-          <div css={{ flex: 'auto', marginLeft: '1rem' }}>
-            <StatusBar />
-            <Editor />
+    <RushProvider value={rush}>
+      <Router>
+        <div>
+          <Global
+            styles={{
+              body: {
+                margin: 0,
+                padding: 0,
+                fontFamily:
+                  '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+              },
+            }}
+          />
+          <div
+            css={{
+              display: 'flex',
+            }}
+          >
+            <ProjectSidebar />
+            <div css={{ flex: 'auto', marginLeft: '1rem' }}>
+              <StatusBar />
+              <Editor />
+            </div>
           </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </RushProvider>
   );
-}
+};

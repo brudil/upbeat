e52f5d925cf72a46508e34f5df74295fd320c509
James Canning <james@brudil.com>
2019-10-30 21:50:21 +0000

    Improve styling of UserDropdown

--- packages/client/src/components/UserDropdown/index.tsx
+++ packages/client/src/components/UserDropdown/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 export const UserDropdown = () => {
   return (
-    <div>
-      <div>Alison Collins</div>
-      <div>Connected</div>
+    <div css={{ flex: '1 0 auto' }}>
+      <div css={{ fontSize: '0.9rem' }}>Alison Collins</div>
+      <div css={{ fontSize: '0.65rem', textTransform: 'uppercase' }}>
+        Connected
+      </div>
     </div>
   );
 };

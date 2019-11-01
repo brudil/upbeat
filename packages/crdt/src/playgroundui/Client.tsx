d934c69fac1e10dac0b61e20f732129d171fc7af
James Canning <james@brudil.com>
2019-11-01 15:12:37 +0000

    Add client network status toggle to playground
    Closes #35

--- packages/crdt/src/playgroundui/Client.tsx
+++ packages/crdt/src/playgroundui/Client.tsx
@@ -34,9 +34,19 @@ export const Client: React.FC<{ clientContainer: ClientContainer }> = ({
     <div className="p-3 bg-blue-100 my-2">
       <Heading>
         <span className="mr-2">{clientContainer.client.siteId}</span>
-        <span className={online ? 'text-green-700 mr-2' : 'text-red-700 mr-2'}>
+        <button
+          className={`uppercase font-bold text-xs underline ${
+            online ? 'text-green-700 mr-2' : 'text-red-700 mr-2'
+          }`}
+          onClick={() =>
+            (clientContainer.status =
+              clientContainer.status === ClientStatus.ONLINE
+                ? ClientStatus.OFFLINE
+                : ClientStatus.ONLINE)
+          }
+        >
           {online ? 'Online' : 'Offline'}
-        </span>
+        </button>
         <span className="text-orange-700 mr-2">
           {clientContainer.operationQueue.length > 0
             ? `${clientContainer.operationQueue.length} in queue`

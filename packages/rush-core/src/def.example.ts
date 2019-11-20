408196e3c4c4ecd7573c0de99205e55b5a0fea02
James Canning <james@brudil.com>
2019-11-20 19:51:01 +0000

    Add API experiment

---
+++ packages/rush-core/src/def.example.ts
@@ -0,0 +1,10 @@
+function createDocument(name: string) {}
+
+function createArrayType() {}
+
+// defines what a composite crdt type can do.
+const rundown = createDocument('rundown', [{ type: 'row', content: {} }]);
+
+// script - store, how? text vs prosemirror schema vs simplified?
+
+// rundown?

50bd69449e1d828556b05e0ad3881c2d53d5b0ce
James Canning <james@brudil.com>
2020-02-08 18:48:53 +0000

    Update deps

--- packages/cue-client/src/containers/EpisodeRundown.tsx
+++ packages/cue-client/src/containers/EpisodeRundown.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import { RundownEditor } from '../components/RundownEditor';
 
 export const EpisodeRundown = () => {
   return (
     <div>
       <Helmet title={'Rundown'} />
       <h1>Rundown</h1>
+      <RundownEditor />
     </div>
   );
 };
--- packages/upbeat-types/src/index.ts
+++ packages/upbeat-types/src/index.ts
@@ -3,3 +3,45 @@ export type UpbeatOp = (...args: unknown[]) => unknown;
 export interface UpbeatApp {
   operations: {};
 }
+
+// interface UpbeatType {
+//   _type: "UpbeatType"
+// }
+
+// interface UpbeatGrowSet extends UpbeatType {
+//   type: "set"
+// }
+
+// interface UpbeatText extends UpbeatType {
+//   type: "text"
+// }
+
+// type UpbeatTypes = UpbeatSet;
+
+// const createLWWMap = () => ({
+//   type: 'lwwmap',
+// });
+
+// // rundown
+// // - list of: items
+// //   - items contain: columns (text/numbers), relations to: scripts/other objects
+
+// /*
+// rundown: {
+//   items: [
+//     {
+//       id,
+//       itemId,
+//       name,
+//       notes,
+//       timeExpected,
+//       addional: {
+//         x,y,z
+//       }
+//     }
+//   ]
+// }
+// */
+
+// const rundown = createEntityContainer('rundown');
+// const rundownItem = createCRDTEntity('rundownItem', );

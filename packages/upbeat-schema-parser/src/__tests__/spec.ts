1286f012f6b014a5029d33b45e7100aeec1624a4
James Canning <james@brudil.com>
2020-02-11 11:39:26 +0000

    Add parser tests

---
+++ packages/upbeat-schema-parser/src/__tests__/spec.ts
@@ -0,0 +1,48 @@
+import { upbeatLexer } from '../lexer';
+import { parseInput } from '../parser';
+
+const SPEC = `
+resource Todo {
+  String name;
+  Boolean complete;
+  CreatedDate createdAt;
+  User user;
+  Tag tags;
+  Int order;
+}
+
+resource Tag {
+  String name;
+  String color;
+}
+
+space Project {
+  Todo todos;
+}
+`;
+
+const BAD_SPEC = `
+res Do_C {
+  String name;
+}
+`;
+
+describe('spec', () => {
+  it('does not fail to lex', () => {
+    const lexingResult = upbeatLexer.tokenize(SPEC);
+    expect(lexingResult.errors.length).toBe(0);
+  });
+
+  it('bad formatting to fail', () => {
+    const parseAst = () => parseInput(BAD_SPEC);
+
+    expect(parseAst).toThrow();
+  });
+
+  it('has found resources and spaces', () => {
+    const ast = parseInput(SPEC);
+
+    expect(ast.spaces.length).toBe(1);
+    expect(ast.resources.length).toBe(2);
+  });
+});

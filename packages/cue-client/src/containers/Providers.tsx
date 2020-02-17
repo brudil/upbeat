5c90d18097bb2eb46c5df21d0f44c1e8e2eaa351
James Canning <james@brudil.com>
2020-02-17 23:48:33 +0000

    Update cue to new upbeat client

--- packages/cue-client/src/containers/Providers.tsx
+++ packages/cue-client/src/containers/Providers.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { HelmetProvider } from 'react-helmet-async';
-import { UpbeatProvider } from '@upbeat/client/src/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createUpbeat } from '@upbeat/client/src/upbeat';
-import { CueApp } from '@withcue/shared/src';
+import { UpbeatProvider } from '../../../upbeat-react/src/react';
+import { createClient } from '@upbeat/client/src/client';
 
-const upbeat = createUpbeat<CueApp>({
+const upbeat = createClient({});
+/*
+{
   uri: 'ws://localhost:8011',
-});
-
+}
+ */
 (window as any).upbeat = upbeat;
 
 export const Providers: React.FC = ({ children }) => {
